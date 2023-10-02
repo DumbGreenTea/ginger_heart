0d7864eb1fad1cbba22d28e9fae715aca50d341b
Diego Leiva Reyes <diegoleivareyes@MacBook-Pro-de-Diego.local>
2023-10-01 21:25:39 -0300

    carrusel de imagenes implementado junto a footer

--- src/Carousel.js
+++ src/Carousel.js
@@ -4,10 +4,13 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import "./CarouselStyle.css"
 
+
 const ImageCarousel = () => {
   const settings = {
-    dots: false, // No mostrar puntos de navegación
+    dots: true, //  mostrar puntos de navegación
     infinite: true, // Bucle infinito
+    autoplay:true,
+    autoplaySpeed:3000,
     speed: 500, // Velocidad de transición
     slidesToShow: 3, // Número de imágenes visibles a la vez
     slidesToScroll: 1, // Número de imágenes para avanzar/retroceder
@@ -17,13 +20,22 @@ const ImageCarousel = () => {
     <div className="image-carousel">
       <Slider {...settings}>
         <div>
-          <img src="public/torta1.png" alt="Imagen 1" />
+          <img src="torta1.jpg" alt="Imagen 1" width="100%"/>
         </div>
         <div>
-          <img src="public/torta2.png" alt="Imagen 2" />
+          <img src="torta2.jpg" alt="Imagen 2" width="100%"/>
         </div>
         <div>
-          <img src="public/torta3.png" alt="Imagen 3" />
+          <img src="torta3.jpg" alt="Imagen 3" width="100%"/>
+        </div>
+        <div>
+          <img src="torta4.png" alt="Imagen 4" width="100%"/>
+        </div>
+        <div>
+          <img src="torta5.png" alt="Imagen 5" width="100%"/>
+        </div>
+        <div>
+          <img src="torta6.png" alt="Imagen 6 " width="100%"/>
         </div>
         {/* Agrega más imágenes aquí */}
       </Slider>
--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,20 @@
 import './App.css';
 import Header from './Navbar'
 import ImageCarousel from './Carousel';
+import Footer from './Footer';
 
 function Home() {
   return (
     <div className="App">
-        <Header/>
-        <div>
-        <h1>Carousel de Imágenes</h1>
-        <ImageCarousel />
-        </div>
-      
+      <Header/>
+      <div className="Middle">
+      <h1>Carousel de Imágenes</h1>
+      <ImageCarousel/>
+      </div>
+      <div className='Bottom'>
+
+      </div>
+      <Footer/>  
     </div>
   );
 }
