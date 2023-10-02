5c0207602d8c6d3d6ea7c3b0ea6fcdae58b24ec7
Diego Leiva Reyes <diegoleivareyes@DESKTOP-KS1CAGG.uai.cl>
2023-10-02 15:44:44 -0300

    pagina home lista en general

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 
-import './App.css';
+import './HomeStyle.css';
 import Header from './Navbar'
 import ImageCarousel from './Carousel';
 import Footer from './Footer';
@@ -13,6 +13,11 @@ function Home() {
       <ImageCarousel/>
       </div>
       <div className='Bottom'>
+        <div className='Blocks'>
+          <div className='block'><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed justo non tortor interdum commodo in in risus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam sit amet facilisis diam. Etiam libero elit, posuere imperdiet vehicula nec, ornare in purus. Ut accumsan libero ullamcorper bibendum rhoncus. Fusce tincidunt diam elit, eget congue dolor laoreet rutrum. In aliquet ex vitae risus venenatis maximus. Nulla quis ipsum in arcu dictum interdum nec vel neque. Quisque a sagittis diam, quis dignissim massa. Vestibulum semper egestas nunc sit amet viverra. Curabitur ultrices massa eu efficitur pharetra. Nullam nunc ex, placerat id convallis a, suscipit eu lorem. Donec sit amet massa est. Duis fermentum mi vitae vehicula dictum. </p></div>
+          <div className='block'><p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed justo non tortor interdum commodo in in risus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam sit amet facilisis diam. Etiam libero elit, posuere imperdiet vehicula nec, ornare in purus. Ut accumsan libero ullamcorper bibendum rhoncus. Fusce tincidunt diam elit, eget congue dolor laoreet rutrum. In aliquet ex vitae risus venenatis maximus. Nulla quis ipsum in arcu dictum interdum nec vel neque. Quisque a sagittis diam, quis dignissim massa. Vestibulum semper egestas nunc sit amet viverra. Curabitur ultrices massa eu efficitur pharetra. Nullam nunc ex, placerat id convallis a, suscipit eu lorem. Donec sit amet massa est. Duis fermentum mi vitae vehicula dictum.</p></div>
+          <div className='block'><p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed justo non tortor interdum commodo in in risus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam sit amet facilisis diam. Etiam libero elit, posuere imperdiet vehicula nec, ornare in purus. Ut accumsan libero ullamcorper bibendum rhoncus. Fusce tincidunt diam elit, eget congue dolor laoreet rutrum. In aliquet ex vitae risus venenatis maximus. Nulla quis ipsum in arcu dictum interdum nec vel neque. Quisque a sagittis diam, quis dignissim massa. Vestibulum semper egestas nunc sit amet viverra. Curabitur ultrices massa eu efficitur pharetra. Nullam nunc ex, placerat id convallis a, suscipit eu lorem. Donec sit amet massa est. Duis fermentum mi vitae vehicula dictum.</p></div>
+        </div>
 
       </div>
       <Footer/>  
